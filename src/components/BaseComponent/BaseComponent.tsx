39e900a30dbf4ca9542d4c66887388beb937ddd9
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-11-29 11:53:00 +0200

    base-component+store

--- src/components/BaseComponent/BaseComponent.tsx
+++ src/components/BaseComponent/BaseComponent.tsx
@@ -1,12 +1,17 @@
 import { Fragment, useEffect, useLayoutEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { noteActions, RootState, useThunkDispatch } from "../../store/store";
+import { exitNoteDialog } from "../../store/action-creators/action-creators";
+import { NoteType } from "../../types/types";
+
 
 type Props = {
     activePage: string,
     pageTitle: { main: string, optional?: string },
     notesUnavailableClass?: string,
 	notesUnavailableInfo: string,
-	notesUnavailableIcon: JSX.Element,
+    notesUnavailableIcon: JSX.Element,
+    notes: NoteType[]
 }
 
 function BaseComponent(props: Props): JSX.Element {
@@ -20,7 +25,7 @@ function BaseComponent(props: Props): JSX.Element {
     } = props
     
     const {
-        isNoteDialogeVisible,
+        isNoteDialogVisible,
         noteTheme,
     } = useSelector((state: RootState) => state)
     
@@ -33,5 +38,53 @@ function BaseComponent(props: Props): JSX.Element {
 	const [deleteConfirmVisible, setDeleteConfirmVisibility] = useState(false);
     const [deleteAmount, setDeleteAmount] = useState("");
     
+    const closeNoteDialog = () => {
+        thunkDispatch(exitNoteDialog(activePage))
+    }
     
+    const showNoteDialog = () => {
+        dispatch(noteActions.noteDialogIsVisible(true))
+    }
+
+	const showDeleteConfirm = (value: boolean) => {
+		setDeleteConfirmVisibility(value);
+	}
+
+	const syncDeleteAmount = (value: string) => {
+		setDeleteAmount(value);
+	}
+
+	useLayoutEffect(() => {
+		if (notes.length > 0) {
+			setNotesUnavailable(false);
+		} else {
+			setNotesUnavailable(true);
+		}
+	}, [notes.length]);
+
+
+	// Visibility of note-dialog overlay
+	useEffect(() => {
+		if (isNoteDialogVisible) {
+			setOverlayClasses("overlay-visible");
+		} else {
+			setOverlayClasses("");
+		}
+	}, [isNoteDialogVisible]);
+
+
+	// Glowing effect behind note-dialog for gradient backgrounds
+	useEffect(() => {
+		if (isNoteDialogVisible && noteTheme.isGradient) {
+			setBlurOverlayClasses(`blur-visible ${noteTheme.colour}`);				
+		} else {
+			setBlurOverlayClasses("");
+		}
+	}, [isNoteDialogVisible, noteTheme]);
+
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
 }
